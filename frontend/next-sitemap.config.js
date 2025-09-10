7c0a122248e17f0062cbc54c084708f80a3fcd51
DaiNam159 <dainam15986@gmail.com>
2025-09-10 13:52:21 +0700

    config sitemap

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "http://localhost:3000", // domain của bạn
-  generateRobotsTxt: true, // tạo luôn robots.txt
+  siteUrl: process.env.SITE_URL || "http://localhost:3000",
+  generateRobotsTxt: true,
   sitemapSize: 7000,
+
+  // Bỏ qua các route không muốn đưa vào sitemap
+  exclude: ["/admin/*"],
+
+  // Tùy chỉnh robots.txt
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin"], // chặn toàn bộ /admin
+      },
+    ],
+    sitemap: `${process.env.SITE_URL || "http://localhost:3000"}/sitemap.xml`,
+  },
 };
